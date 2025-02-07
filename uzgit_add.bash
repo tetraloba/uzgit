e628199f94c0570dccccd732651c57e9e5a1ee16
tetraloba <four.leaves.siva7342@gmail.com>
2025-02-07 19:29:46 +0900

    uzgit_test

---
+++ uzgit_add.bash
@@ -0,0 +1,5 @@
+FILE_PATH=$1
+rm -r ${FILE_PATH}_unziped_
+unzip $FILE_PATH -d ${FILE_PATH}_unziped_
+git add ${FILE_PATH}_unziped_
+
---
+++ uzgit_test.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EXPORT_DIR=all_versions
+
+REPO=research_B
+
+FILE_BASE_PATH=21T314
+FILE_EXT=docx
+
+TEMP_REPO_DIR=tmp_dir_uzgit
+
+mkdir ${TEMP_REPO_DIR}
+cd ${TEMP_REPO_DIR}
+git init
+ls ../${EXPORT_DIR} | while read NEW_FILE; do
+    cp ../${EXPORT_DIR}/${NEW_FILE} ${FILE_BASE_PATH}.${FILE_EXT}
+    bash ../uzgit_add.bash ${FILE_BASE_PATH}.${FILE_EXT}
+    git commit -m ${NEW_FILE}
+    du . -d 0 >> size_log.txt
+done
+
